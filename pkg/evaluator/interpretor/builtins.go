c0c6e9ab264e199e8579f05e138f5b3f1d200bb2
Latiif <latiif@mail.com>
2020-06-23 19:59:38 +0200

    Add out, head, tail and typeof built-in functions
    - out: writes string represenation of its arguments to standard output.
    - head: expects 1 arg of type array, returns first element.
            head([]) == null.
    - tail: expects 1 arg of type array, returns array excluding
            the first element. tail([]) == [].
    - typeof: expects 1 arg of any type, returns string reflecting the
              arguments type.

--- pkg/evaluator/interpretor/builtins.go
+++ pkg/evaluator/interpretor/builtins.go
@@ -1,11 +1,70 @@
 package interpretor
 
-import "github.com/latiif/lail/pkg/object"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/latiif/lail/pkg/object"
+)
 
 var builtins = map[string]*object.Builtin{
-	"len": {
+	"out": {
 		Function: func(args ...object.Object) object.Object {
-			return Null
+			var out bytes.Buffer
+
+			for _, arg := range args {
+				out.WriteString(arg.Inspect())
+			}
+
+			fmt.Println(out.String())
+			return &object.String{
+				Value: out.String(),
+			}
+		},
+	},
+	"head": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newIllegalStateException(fmt.Sprintf("head takes 1 argument; %d were provided.", len(args)))
+			}
+			array, ok := args[0].(*object.Array)
+			if !ok {
+				return newIllegalStateException(fmt.Sprintf("%s is not an array literal.", args[0].Inspect()))
+			}
+			// head of [] is Null
+			if len(array.Value) == 0 {
+				return Null
+			}
+			return array.Value[0]
+		},
+	},
+	"tail": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newIllegalStateException(fmt.Sprintf("tail takes 1 argument; %d were provided.", len(args)))
+			}
+			array, ok := args[0].(*object.Array)
+			if !ok {
+				return newIllegalStateException(fmt.Sprintf("%s is not an array literal.", args[0].Inspect()))
+			}
+			// return empty array
+			// tail of [] is []
+			if len(array.Value) == 0 {
+				return &object.Array{}
+			}
+			return &object.Array{
+				Value: array.Value[1:],
+			}
+		},
+	},
+	"typeof": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newIllegalStateException(fmt.Sprintf("typeof takes 1 argument; %d were provided.", len(args)))
+			}
+			return &object.String{
+				Value: string(args[0].Type()),
+			}
 		},
 	},
 }
